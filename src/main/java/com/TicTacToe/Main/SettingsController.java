22d1f8f86b183f794b4cc137eaf7f0208677bdff
Denis Rozimovschii <rozimovschii.denis@yahoo.com>
2016-10-14 10:58:06 +0300

    Fixed null settings from the start

--- src/main/java/com/TicTacToe/Main/SettingsController.java
+++ src/main/java/com/TicTacToe/Main/SettingsController.java
@@ -14,148 +14,151 @@ import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
 public class SettingsController {
-    public Slider errorSlider;
-    public Slider shuffleSlider;
-    public Label errorLabel;
-    public Label shuffleLabel;
-    public double currentError;
-    public double currentShuffle;
-    public Stage mainStage;
-    public Scene mainScene;
+	public Slider errorSlider;
+	public Slider shuffleSlider;
+	public Label errorLabel;
+	public Label shuffleLabel;
+	public double currentError;
+	public double currentShuffle;
+	public Stage mainStage;
+	public Scene mainScene;
 
-    public String path;
-    public void initialize() {
-	
-	initSaveDirectory();
-	
-	read();
+	public String path;
+	public void initialize() {
 
-	update();
-	
-    }
+		initSaveDirectory();
 
-    public void initSaveDirectory() {
-	
-	path = System.getProperty("user.home") + "/TicTacToeFX";
-	File loc = new File(path);
+		read();
 
-	if (!loc.isDirectory()) {
-	    try {
-		loc.mkdirs();
-	    } catch (Exception e) {
-		Logger.error("Error creating a directory at " + loc.getPath());
-	    }
-	}
-
-	File file = new File(path + "/settings.txt");
-	try {
-	    
-	    if (!file.exists()) {
-		file.createNewFile();
-		write(0.0, 50.0);
-	    }		    
-	    
-	} catch (Exception e) {
-	    Logger.error("Error creating a file at " + file.getPath());
-	}
-	
-    }
-
-    private void read() {
-
-	File file = new File(path + "/settings.txt");
-	Scanner sc = null;
-	try {
-	    sc = new Scanner(file).useLocale(Locale.ENGLISH);
-	    
-	    currentError = sc.nextDouble();
-	    currentShuffle = sc.nextDouble();
-
-	    sc.close();
-
-	    update();
-	} catch (Exception e) {
-	    Logger.error("There was an error reading the settings file");
-	    e.printStackTrace();
-	}
-    }
-
-    private void write() {
-
-	File file = new File(path + "/settings.txt");
-	PrintWriter pw = null;
-	
-	try {
-	    pw = new PrintWriter(new FileWriter(file));
-	    pw.println(currentError);
-	    pw.println(currentShuffle);
-	    pw.close();
-	    update();
-	} catch (Exception e) {
-	    Logger.error("There was an error writing to the settings file");
-	    e.printStackTrace();
-	} finally {
+		update();
+		
+		currentError = 0;
+		currentShuffle = 50;
 
 	}
-    }
 
-    private void write(double currentError, double currentShuffle) {
+	public void initSaveDirectory() {
 
-	File file = new File(path + "/settings.txt");
-	PrintWriter pw = null;
-	
-	try {
-	    pw = new PrintWriter(new FileWriter(file));
-	    pw.println(currentError);
-	    pw.println(currentShuffle);
-	    pw.close();
-	    update();
-	} catch (Exception e) {
-	    Logger.error("There was an error writing to the settings file");
-	    e.printStackTrace();
-	} finally {
+		path = System.getProperty("user.home") + "/TicTacToeFX";
+		File loc = new File(path);
+
+		if (!loc.isDirectory()) {
+			try {
+				loc.mkdirs();
+			} catch (Exception e) {
+				Logger.error("Error creating a directory at " + loc.getPath());
+			}
+		}
+
+		File file = new File(path + "/settings.txt");
+		try {
+
+			if (!file.exists()) {
+				file.createNewFile();
+				write(0.0, 50.0);
+			}		    
+
+		} catch (Exception e) {
+			Logger.error("Error creating a file at " + file.getPath());
+		}
 
 	}
-    }
-    private void update() {
-	errorLabel.setText(Double.toString(Math.round(currentError)) + "%");
-	shuffleLabel.setText(Double.toString(Math.round(currentShuffle)) + "%");
 
-	errorSlider.setValue(currentError);
-	shuffleSlider.setValue(currentShuffle);
-    }
+	private void read() {
 
-    public void setStage(Stage mainStage) {
-	this.mainStage = mainStage;
-    }
+		File file = new File(path + "/settings.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file).useLocale(Locale.ENGLISH);
 
-    public void setMainSceneScene(Scene mainScene) {
-	this.mainScene = mainScene;
-    }
+			currentError = sc.nextDouble();
+			currentShuffle = sc.nextDouble();
 
-    public void backToMenuPressed() {
+			sc.close();
 
-	mainStage.setScene(mainScene);
-    }
+			update();
+		} catch (Exception e) {
+			Logger.error("There was an error reading the settings file");
+			e.printStackTrace();
+		}
+	}
 
-    public void savePressed() {
+	private void write() {
 
-	write();
+		File file = new File(path + "/settings.txt");
+		PrintWriter pw = null;
 
-    }
+		try {
+			pw = new PrintWriter(new FileWriter(file));
+			pw.println(currentError);
+			pw.println(currentShuffle);
+			pw.close();
+			update();
+		} catch (Exception e) {
+			Logger.error("There was an error writing to the settings file");
+			e.printStackTrace();
+		} finally {
 
-    public void errorSliderDragged() {
+		}
+	}
 
-	currentError = errorSlider.getValue();
+	private void write(double currentError, double currentShuffle) {
 
-	update();
+		File file = new File(path + "/settings.txt");
+		PrintWriter pw = null;
 
-    }
+		try {
+			pw = new PrintWriter(new FileWriter(file));
+			pw.println(currentError);
+			pw.println(currentShuffle);
+			pw.close();
+			update();
+		} catch (Exception e) {
+			Logger.error("There was an error writing to the settings file");
+			e.printStackTrace();
+		} finally {
 
-    public void shuffleSliderDragged() {
+		}
+	}
+	private void update() {
+		errorLabel.setText(Double.toString(Math.round(currentError)) + "%");
+		shuffleLabel.setText(Double.toString(Math.round(currentShuffle)) + "%");
 
-	currentShuffle = shuffleSlider.getValue();
+		errorSlider.setValue(currentError);
+		shuffleSlider.setValue(currentShuffle);
+	}
 
-	update();
-    }
+	public void setStage(Stage mainStage) {
+		this.mainStage = mainStage;
+	}
+
+	public void setMainSceneScene(Scene mainScene) {
+		this.mainScene = mainScene;
+	}
+
+	public void backToMenuPressed() {
+
+		mainStage.setScene(mainScene);
+	}
+
+	public void savePressed() {
+
+		write();
+
+	}
+
+	public void errorSliderDragged() {
+
+		currentError = errorSlider.getValue();
+
+		update();
+
+	}
+
+	public void shuffleSliderDragged() {
+
+		currentShuffle = shuffleSlider.getValue();
+
+		update();
+	}
 }
